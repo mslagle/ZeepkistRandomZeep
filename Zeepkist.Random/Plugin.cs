26cd54dded83c116af5004649410ee5c7c93a2e6
Matt Slagle <Matt.Slagle@onbase.com>
2024-07-03 10:49:49 -0400

    Feature:  v16 randomizer

--- Zeepkist.Random/Plugin.cs
+++ Zeepkist.Random/Plugin.cs
@@ -100,45 +100,82 @@ namespace Zeepkist.RandomZeep
 
         public static void Randomize()
         {
-            PlayerManager.Instance.messenger.Log("Randomizing zeep", 1.0f);
-
-            var hats = CosmeticsApi.GetUnlockedHats();
-            var carts = CosmeticsApi.GetUnlockedZeepkists();
-            var colors = CosmeticsApi.GetUnlockedColors();
-            Debug.Log($"Randomizing zeep character with {hats.Count} hats, {carts.Count} carts, and {colors.Count} colors");
-
-            var selectedHat = hats[RandomNumberGenerator.GetInt32(hats.Count)];
-            var selectedCart = carts[RandomNumberGenerator.GetInt32(carts.Count)];
-            var selectedColor = colors[RandomNumberGenerator.GetInt32(colors.Count)];
-            Debug.Log($"Applying {selectedHat.name} hat, {selectedCart.name} cart, and {selectedColor.name} color...");
-
-            PlayerManager.Instance.avontuurSoapbox = selectedCart;
-            PlayerManager.Instance.avontuurColor = selectedColor;
-            PlayerManager.Instance.avontuurHat = selectedHat;
+            var cosmetics = RandomizeCosmetics();
 
             // Reload during the main menu, online, or offline
             if (modelCar != null && modelCar.soapbox != null)
             {
-                modelCar.DoCarSetup(selectedCart, selectedHat, selectedColor, true, false, false);
+                modelCar.DoCarSetup(PlayerManager.Instance.adventureCosmetics, true, false, false);
             }
-            // Reload only duriing online - backup call only
+
+            // Reload only during online - backup call only
             else if (PlayerManager.Instance?.currentMaster?.carSetups?.FirstOrDefault<SetupCar>() != null)
             {
-                PlayerManager.Instance.currentMaster.carSetups.FirstOrDefault<SetupCar>().SetupSoapbox(selectedCart);
-                PlayerManager.Instance.currentMaster.carSetups.FirstOrDefault<SetupCar>().SetupCharacter(selectedHat, selectedColor);
+                PlayerManager.Instance.currentMaster.carSetups.FirstOrDefault<SetupCar>(x => x.tag == modelCar.tag).SetupSoapbox(PlayerManager.Instance.adventureCosmetics);
+                PlayerManager.Instance.currentMaster.carSetups.FirstOrDefault<SetupCar>(x => x.tag == modelCar.tag).SetupCharacter(PlayerManager.Instance.adventureCosmetics);
             }
 
             // Send a packet to update in multiplayer
             if (ZeepkistNetwork.IsConnectedToGame)
             {
+
+
                 PlayerCosmeticsPacket packet = new PlayerCosmeticsPacket();
-                packet.ZeepkistID = PlayerManager.Instance.objectsList.wardrobe.GetZeepkistUnlocked(PlayerManager.Instance.avontuurSoapbox.GetCompleteID()).GetCompleteID();
-                packet.HatID = PlayerManager.Instance.objectsList.wardrobe.GetHatUnlocked(PlayerManager.Instance.avontuurHat.GetCompleteID()).GetCompleteID();
-                packet.ColorID = PlayerManager.Instance.objectsList.wardrobe.GetColorUnlocked(PlayerManager.Instance.avontuurColor.GetCompleteID()).GetCompleteID();
+                packet.cosmetics = PlayerManager.Instance.adventureCosmetics.GetIDs();
+                packet.chatColor = Color.HSVToRGB(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
                 ZeepkistNetwork.NetworkClient?.SendPacket<PlayerCosmeticsPacket>(packet);
             }
 
             Debug.Log("Finished applying randomized cosmetics");
+        }
+
+        public static CosmeticsV16 RandomizeCosmetics()
+        {
+            PlayerManager.Instance.messenger.Log("Randomizing zeep", 1.0f);
+
+            var carts = CosmeticWardrobe.Instance.unlockedZeepkist.Values.Cast<Object_Soapbox>().ToList();
+            var hats = CosmeticWardrobe.Instance.unlockedHat.Values.Cast<HatValues>().ToList();
+            var colors = CosmeticWardrobe.Instance.unlockedColor.Values.Cast<CosmeticColor>().ToList();
+            var glasses = CosmeticWardrobe.Instance.unlockedGlasses.Values.Cast<HatValues>().ToList();
+            var wheels = CosmeticWardrobe.Instance.unlockedWheel.Values.Cast<Object_Wheel>().ToList();
+            var gliders = CosmeticWardrobe.Instance.unlockedParaglider.Values.Cast<Object_Paraglider>().ToList();
+            var horns = CosmeticWardrobe.Instance.unlockedHorn.Values.Cast<Object_Horn>().ToList();
+            Debug.Log($"Randomizing with {carts.Count} carts, {hats.Count} hats, {colors.Count} colors, {glasses.Count} glasses, {wheels.Count} wheels, {gliders.Count} paragliders, {horns.Count} horns.");
+
+            var selectedCart = carts[RandomNumberGenerator.GetInt32(carts.Count)];
+            var selectedHat = hats[RandomNumberGenerator.GetInt32(hats.Count)];
+            var selectedGlasses = glasses[RandomNumberGenerator.GetInt32(glasses.Count)];
+            var selectedGlider = gliders[RandomNumberGenerator.GetInt32(gliders.Count)];
+            var selectedHorn = horns[RandomNumberGenerator.GetInt32(horns.Count)];
+            Debug.Log($"Applying {selectedCart.name} cart, {selectedHat.name} hat, {selectedGlasses.name} glasses, {selectedGlider.name} glider, and {selectedHorn.name} horn...");
+
+            var selectedColorLeftLeg = colors[RandomNumberGenerator.GetInt32(colors.Count)];
+            var selectedColorRightLeg = colors[RandomNumberGenerator.GetInt32(colors.Count)];
+            var selectedColorLeftArm = colors[RandomNumberGenerator.GetInt32(colors.Count)];
+            var selectedColorRightArm = colors[RandomNumberGenerator.GetInt32(colors.Count)];
+            var selectedColorBody = colors[RandomNumberGenerator.GetInt32(colors.Count)];
+            Debug.Log($"Applying {selectedColorLeftLeg.name} left leg, {selectedColorRightLeg.name} right leg, {selectedColorLeftArm.name} left arm, {selectedColorRightArm.name} right arm, and {selectedColorBody.name} body...");
+
+            var selectedFrontWheels = wheels[RandomNumberGenerator.GetInt32(wheels.Count)];
+            var selectedRearWheels = wheels[RandomNumberGenerator.GetInt32(wheels.Count)];
+            Debug.Log($"Applying {selectedFrontWheels.name} front wheels, and {selectedRearWheels.name} rear wheels...");
+
+            PlayerManager.Instance.adventureCosmetics.zeepkist = selectedCart;
+            PlayerManager.Instance.adventureCosmetics.hat = selectedHat;
+            PlayerManager.Instance.adventureCosmetics.glasses = selectedGlasses;
+            PlayerManager.Instance.adventureCosmetics.paraglider = selectedGlider;
+            PlayerManager.Instance.adventureCosmetics.horn = selectedHorn;
+
+            PlayerManager.Instance.adventureCosmetics.color_body = selectedColorBody;
+            PlayerManager.Instance.adventureCosmetics.color_leftArm = selectedColorLeftArm;
+            PlayerManager.Instance.adventureCosmetics.color_rightArm = selectedColorRightArm;
+            PlayerManager.Instance.adventureCosmetics.color_leftLeg = selectedColorLeftLeg;
+            PlayerManager.Instance.adventureCosmetics.color_rightLeg = selectedColorRightLeg;
+
+            PlayerManager.Instance.adventureCosmetics.frontwheels = selectedFrontWheels;
+            PlayerManager.Instance.adventureCosmetics.rearwheels = selectedRearWheels;
+
+            return PlayerManager.Instance.adventureCosmetics;
         }
 
         public void OnDestroy()
